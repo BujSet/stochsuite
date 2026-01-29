b5e76728407e6a4f913ec7347d109d5476268aae
BujSet <ranganath1000@gmail.com>
2026-01-28 18:35:12 -0600

    Trying to add in multi-platform support to include arm based platforms

--- stochsuite.mk
+++ stochsuite.mk
@@ -1,5 +1,7 @@
 RNG_LIB := $(STOCHSUITE_HOME)/rngs/software
 RNG_SRCS := $(wildcard $(RNG_LIB)/*.cpp) 
 CPP := g++
-CFLAGS := -c -std=c++11 -Wall -O3 -lm
+COMMON_CFLAGS := -c -std=c++11 -Wall -O3 -lm
+PLATFORM_CFLAGS := 
 LFLAGS := -std=c++11 -Wall -O3
+UNAME := $(shell uname)
