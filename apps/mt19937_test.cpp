0d90bab5f9f549e80f9dbc27ce2c6ff1afd49ace
BujSet <ranganath1000@gmail.com>
2026-01-30 17:28:37 -0600

    Starter code to test mt19937 software version matches expected behavior

---
+++ apps/mt19937_test.cpp
@@ -0,0 +1,71 @@
+#include "RNGFactory.hpp"
+#include "RandomNumberGenerator.hpp"
+#include "MersenneTwister.h"
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <limits>
+
+int main(int argc, char *argv[]) {
+    MTRand *randPtr = new MTRand();
+    std::cout << "Created MTRand Object" << std::endl;
+
+    delete randPtr;
+	return 0;
+
+    size_t niters = 100;
+    uint32_t seed = 12312332;
+    std::string rngStr = "Taus88";
+
+    if (argc > 1) {
+        if (argc > 7) {
+            std::cout << "usage: ./" << argv[0] << " -seed <SEED> ";
+            std::cout << " -iters <NUM_ITERS> -rng <RNG>" << std::endl;
+            return 1;
+        }
+        for (size_t i = 0; i < (size_t)argc; i++) {
+            if (strcmp("-seed", argv[i]) == 0) {
+                assert(i + 1 < (size_t)argc);
+                seed = (uint32_t) strtol(argv[i+1], NULL, 10);
+                i++;
+            }
+            if (strcmp("-iters", argv[i]) == 0) {
+                assert(i + 1 < (size_t)argc);
+                niters = (size_t) strtol(argv[i+1], NULL, 10);
+                i++;
+            }
+            if (strcmp("-rng", argv[i]) == 0) {
+                assert(i + 1 < (size_t)argc);
+                rngStr = (std::string) argv[i+1];
+                i++;
+            }
+        }
+    }
+    double x, y, z, pi;
+    size_t count = 0;
+
+    std::unique_ptr<RNGBase> rng = RNGFactory::createRNG(rngStr);
+    if (!rng) {
+        std::cout << "Failed to instantiate RNG instance!" << std::endl;
+        return 1;
+    }
+    std::cout << "Successfully initialized RNG: " << rng->name() << std::endl;
+
+    rng->seed_random(seed);
+
+    // Begin ROI
+    for (size_t i = 0; i < niters; i++) {
+        x = rng->read_random_double();
+        y = rng->read_random_double();
+        z = sqrt((x*x) + (y*y));
+        if (z <= 1.0) {
+            count++;
+        }
+    }
+    pi = 4.0 * ((double)count) / ((double)niters);
+    // End ROI
+    
+    std::cout << "pi = " << pi << std::endl;
+	return 0;
+}
