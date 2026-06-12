67afbc776cf26449e454644445ef0a2b61140713
BujSet <ranganath1000@gmail.com>
2026-06-12 17:33:11 -0500

    Able to run OpenSTA for get some initial critical path info

---
+++ rngs/hardware/run_sta.tcl
@@ -0,0 +1,13 @@
+set_cmd_units -time ps -capacitance ff -current mA -voltage V -resistance kOhm
+
+# OpenSTA script (run_sta.tcl)
+read_liberty vsclib013.lib
+read_verilog lcg_synthesized_netlist.v
+link_design lcg
+
+# 3. Define your clock constraint (using 4200 ps for a 4.2 ns clock cycle)
+create_clock -name my_clk -period 4200 [get_ports clk]
+
+report_checks -path_delay max -fields {slew cap input pin delay attr}
+exit
+
